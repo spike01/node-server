45929659672017937dbea75e5fbd2acedeec8b64
Spike Nox <spike01@gmail.com>
2014-11-08 18:03:58 +0000

    Ready for Sinatra integration

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http)
 
 var expressLayouts = require('express-ejs-layouts')
 
-var io = require('./app/sockets.js');
 var port = process.env.PORT || 3000;
 
 app.use(expressLayouts)
@@ -14,14 +14,23 @@ app.set('view engine', 'ejs');
 
 app.get('/widget', function(request, response){
   response.render('widget');
+  console.log('GET widget');
 });
 
 app.get('/settings', function(request, response){
   response.render('settings');
+  console.log('GET settings');
 });
 
 http.listen(port, function(){
   console.log('listening on port ' + port);
 });
 
-module.exports = http;
+
+io.on('connection', function(socket){
+
+  socket.on('message', function(message){
+    io.emit('message', message);
+  });
+});
+
