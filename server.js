e1870833ee48291cee3178288ccf1e52b9a68269
Spike Nox <spike01@gmail.com>
2014-11-08 17:13:09 +0000

    Serves basic Hello World in Wix editor

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
-var io = require('./src/sockets.js');
+
+var expressLayouts = require('express-ejs-layouts')
+
+var io = require('./app/sockets.js');
 var port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-
+app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'))
+
+app.set('view engine', 'ejs');
 
 app.get('/widget', function(request, response){
   response.render('widget');
@@ -15,7 +19,6 @@ app.get('/widget', function(request, response){
 app.get('/settings', function(request, response){
   response.render('settings');
 });
-
 
 http.listen(port, function(){
   console.log('listening on port ' + port);
