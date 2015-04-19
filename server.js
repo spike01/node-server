8c66c77d37a6431dd2dc21d9d4c7e649eb2f062c
Spike Nox <spike01@gmail.com>
2015-04-19 15:27:51 +0100

    Spring cleaning

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(expressLayouts)
 
   app.get('/widget/:constant', function(request, response){
     var text = 'http://wixtextsupport.herokuapp.com/' + request.params.id
-      var name = request.query.name;
-    response.render('widget', { user: "'"+name+"'", content: name });
+    var name = request.query.name;
+  response.render('widget', { user: "'"+name+"'", content: name });
   });
 
 app.get('/owner', function(request, response){
@@ -35,7 +35,7 @@ app.get('/welcome', function(request, response){
   var text = 'http://wixtextsupport.herokuapp.com/owner'
   sendMail();
 //sendText(text);
-  console.log('GET welcome');
+console.log('GET welcome');
 });
 
 
@@ -84,10 +84,10 @@ function sendText(text){
     if (!err) { // "err" is an error received during the request, if any
 
       //"responseData" is a JavaScript object containing data received from Twilio.
-        //A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-          //http://www.twilio.com/docs/api/rest/sending-sms#example-1
+      //A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+      //http://www.twilio.com/docs/api/rest/sending-sms#example-1
 
-          console.log(responseData.from); // outputs "+14506667788"
+      console.log(responseData.from); // outputs "+14506667788"
       console.log(responseData.body); // outputs "word to your mother."
 
     }
